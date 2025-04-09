8cafbfb5d65d6a557b906c2a9ac13e8f7d205280
VEKARICH <wesandoval43@gmail.com>
2025-04-09 14:17:53 -0500

    Se hace la conexión entre el backend y el frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import Login from "./components/Login";
 import ProtectedPage from "./components/ProtectedPage";
+import StatusCheck from "./components/StatusCheck";
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
 
   return (
     <div>
+      {/* Muestra el login si no hay token, o la página protegida si sí hay */}
       {!token ? (
         <Login onLogin={setToken} />
       ) : (
         <ProtectedPage />
       )}
+
+      {/* Este componente hace una prueba de conexión con la API */}
+      <StatusCheck />
     </div>
   );
 }
